8103752356caab376f0e92ae2761f7a259a048b5
Alex Malyutin <alex.malyutin.dev@gmail.com>
2022-08-10 17:50:16 +0300

    Add dvd screen saver on canvas.

--- src/app.ts
+++ src/app.ts
@@ -6,3 +6,35 @@ var result = parseCard({
 });
 
 console.log(result);
+
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+
+var x = 0, y = 0;
+var speed = 2;
+var dx = speed, dy = speed;
+var width = 160, height = 90;
+
+const image = new Image(width, height);
+
+function Update() {
+    ctx.clearRect(0, 0, 900, 600);
+
+    ctx.drawImage(image, x, y, width, height);
+
+    x += dx;
+    y += dy;
+
+    if (x + width > 900) dx = -speed;
+    if (x < 0) dx = speed;
+
+    if (y + height > 600) dy = -speed;
+    if (y < 0) dy = speed;
+
+    requestAnimationFrame(Update);
+}
+
+image.onload = () => {    
+    requestAnimationFrame(Update);
+}
+image.src = "https://www.freepnglogos.com/uploads/dvd-png/tweaking-allm-the-difference-between-blu-ray-and-dvd-30.png";
